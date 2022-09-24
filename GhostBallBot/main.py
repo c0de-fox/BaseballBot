67a018f897397abd8101930acd7688635deb5d5c
c0de <c0de@c0de.dev>
2022-09-23 21:40:51 -0500

    Add the bot entrypoint

---
+++ GhostBallBot/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Copyright 2022 - c0de <c0de@c0de.dev>
+# Licensed under the MIT License (https://opensource.org/licenses/MIT)
+
+import os
+from pathlib import Path
+
+from .discord_client import client
+from .database.models import DATABASE, database, create_models
+
+if __name__ == '__main__':
+    client = GhostBallClient()
+    client.run(os.environ.get('discord_token'))
+
+    # Set up the database if we haven't already
+    if not os.path.exists(DATABASE):
+        database.connect()
+        create_models()
+        database.close()
