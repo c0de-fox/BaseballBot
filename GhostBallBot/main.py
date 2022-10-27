b0c6b8e5bb4f926b7f42c28967e9f8441ec474fc
c0de <c0de@c0de.dev>
2022-10-26 23:35:52 -0500

    Create database before starting the client

--- GhostBallBot/main.py
+++ GhostBallBot/main.py
@@ -10,11 +10,11 @@ from discord_client.client import GhostBallClient
 from database.models import DATABASE, database, create_models
 
 if __name__ == "__main__":
-    client = GhostBallClient(intents=None)
-    client.run(os.environ.get("discord_token"))
-
     # Set up the database if we haven't already
     if not os.path.exists(DATABASE):
         database.connect()
         create_models()
         database.close()
+
+    client = GhostBallClient(intents=None)
+    client.run(os.environ.get("discord_token"))
